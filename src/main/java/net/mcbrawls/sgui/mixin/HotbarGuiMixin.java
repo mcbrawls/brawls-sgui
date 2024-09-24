b7d13d1834f6d4c990d6b7d5b6b703cefb5617a9
Daniel Orr <orrdaniel73@gmail.com>
2024-09-24 07:49:52 +0100

    Hotbar selected slot fix

---
+++ src/main/java/net/mcbrawls/sgui/mixin/HotbarGuiMixin.java
@@ -0,0 +1,31 @@
+package net.mcbrawls.sgui.mixin;
+
+import eu.pb4.sgui.api.gui.BaseSlotGui;
+import eu.pb4.sgui.api.gui.HotbarGui;
+import net.minecraft.server.network.ServerPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(HotbarGui.class)
+public abstract class HotbarGuiMixin extends BaseSlotGui {
+    @Shadow public abstract void setSelectedSlot(int value);
+
+    private HotbarGuiMixin(ServerPlayerEntity player, int size) {
+        super(player, size);
+    }
+
+    @Override
+    public void afterOpen() {
+        setSelectedSlot(player.getInventory().selectedSlot);
+    }
+
+    @Inject(method = "onSelectedSlotChange", at = @At("HEAD"), cancellable = true)
+    private void onSelectedSlotChange(int slot, CallbackInfoReturnable<Boolean> cir) {
+        setSelectedSlot(slot);
+        player.getInventory().selectedSlot = slot;
+        cir.setReturnValue(true);
+    }
+}
