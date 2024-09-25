9c2cea9bbefd49ac6d2f9adfb8c40acc11885d44
Daniel Orr <orrdaniel73@gmail.com>
2024-09-25 22:55:34 +0100

    Fix hotbar slot lag fix

--- src/main/java/net/mcbrawls/sgui/mixin/HotbarGuiMixin.java
+++ src/main/java/net/mcbrawls/sgui/mixin/HotbarGuiMixin.java
@@ -3,6 +3,7 @@ package net.mcbrawls.sgui.mixin;
 import eu.pb4.sgui.api.gui.BaseSlotGui;
 import eu.pb4.sgui.api.gui.HotbarGui;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +13,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(HotbarGui.class)
 public abstract class HotbarGuiMixin extends BaseSlotGui {
     @Shadow(remap = false) public abstract void setSelectedSlot(int value);
+
+    @Shadow protected int selectedSlot;
 
     private HotbarGuiMixin(ServerPlayerEntity player, int size) {
         super(player, size);
@@ -24,8 +27,9 @@ public abstract class HotbarGuiMixin extends BaseSlotGui {
 
     @Inject(method = "onSelectedSlotChange", at = @At("HEAD"), cancellable = true, remap = false)
     private void onSelectedSlotChange(int slot, CallbackInfoReturnable<Boolean> cir) {
-        setSelectedSlot(slot);
-        player.getInventory().selectedSlot = slot;
+        int trueSlot =  MathHelper.clamp(slot, 0, 8);
+        this.selectedSlot = trueSlot;
+        player.getInventory().selectedSlot = trueSlot;
         cir.setReturnValue(true);
     }
 }
